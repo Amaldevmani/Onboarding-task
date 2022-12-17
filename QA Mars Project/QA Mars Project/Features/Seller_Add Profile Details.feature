e3a18fbf6860df482d052b4db33c2998e8a3eebc
amaldev <amaldevmani@yahoo.com>
2022-12-17 13:33:42 +1300

    Task2 completion

--- QA Mars Project/QA Mars Project/Features/Seller_Add Profile Details.feature
+++ QA Mars Project/QA Mars Project/Features/Seller_Add Profile Details.feature
@@ -5,41 +5,41 @@ I want the feature to add my Profile Details
 So that
 The people seeking for some skills can look into my details.
 
+@Test(priority=1)
 Scenario: User updates the profile name, availability, hours and earn target in his/her profile
 	Given User is logged into Mars application successfully
 	When User navigate to profile page
 	And User update the profile name, availability, hours and earn target 
 	Then the profile should be updated successfully 
-	
 
-#Scenario: User updates the description in his/her profile
-	#Given User is logged into Mars application successfully
-	#When User navigate to profile page
-	#And User update the description 
-	#Then the profile should be updated successfully 
+@Test(priority=2)
+Scenario: User updates the description in his/her profile
+	Given User is logged into Mars application successfully
+	When User navigate to profile page
+	And User update the description 
+	Then the profile description should be updated successfully 
 
-
-#Scenario: User updates the languages in his/her profile
-	#Given User is logged into Mars application successfully
-	#When User navigate to profile page
-	#And User update the languages 
-	#Then the profile should be updated successfully 
+@Test(priority=3)
+Scenario: User updates the languages in his/her profile
+	Given User is logged into Mars application successfully
+	When User navigate to profile page
+	And User update the languages 
+	Then the profile language should be updated successfully 
 
 #Scenario: User updates the Skills in his/her profile
 	#Given User is logged into Mars application successfully
 	#When User navigate to profile page
 	#And User update the Skills 
-	#Then the profile should be updated successfully 
+	#Then the profile skills should be updated successfully 
 
 #Scenario: User updates the education in his/her profile
 	#Given User is logged into Mars application successfully
 	#When User navigate to profile page
 	#And User update the education 
-	#Then the profile should be updated successfully 
+	#Then the profile education should be updated successfully 
 
-#@tag1
 #Scenario: User updates the certifications in his/her profile
 	#Given User is logged into Mars application successfully
 	#When User navigate to profile page
 	#And User update the certifications 
-	#Then the profile should be updated successfully 
+	#Then the profile certifications should be updated successfully 
--- QA Mars Project/QA Mars Project/Pages/ProfilePage.cs
+++ QA Mars Project/QA Mars Project/Pages/ProfilePage.cs
@@ -40,6 +40,7 @@ namespace QA_Mars_Project.Pages
 
         public void AddAvailability(IWebDriver driver)
         {
+            waitObj.WaitForAjax(driver);
             IWebElement availabilityWriteIcon = driver.FindElement(By.XPath("//div[@class='extra content']//div//div[2]//i[contains(@class,'write icon')]"));
             availabilityWriteIcon.Click();
             IWebElement availabiltyValue = driver.FindElement(By.XPath("//select//option[text()='Part Time']"));
@@ -49,6 +50,7 @@ namespace QA_Mars_Project.Pages
 
         public void AddHours(IWebDriver driver)
         {
+            waitObj.WaitForAjax(driver);
             IWebElement hoursWriteIcon = driver.FindElement(By.XPath("//div[@class='extra content']//div//div[3]//i[contains(@class,'write icon')]"));
             hoursWriteIcon.Click();
             IWebElement hoursValue = driver.FindElement(By.XPath("//select//option[text()='As needed']"));
@@ -58,6 +60,7 @@ namespace QA_Mars_Project.Pages
 
         public void AddEarnTarget(IWebDriver driver)
         {
+            waitObj.WaitForAjax(driver);
             IWebElement earnTargetWriteIcon = driver.FindElement(By.XPath("//div[@class='extra content']//div//div[4]//i[contains(@class,'write icon')]"));
             earnTargetWriteIcon.Click();
             IWebElement earnTargetValue = driver.FindElement(By.XPath("//select//option[text()='More than $1000 per month']"));
@@ -73,26 +76,84 @@ namespace QA_Mars_Project.Pages
 
             if (titleValue == "Test Name")
             {
-                Console.WriteLine("Profile Name updated successfully");
+                Console.WriteLine("Profile Name updated successfully -Verified");
             }
-            IWebElement PartTimeAvailability = driver.FindElement(By.XPath("//span[text()='Part Time']"));
-            if(PartTimeAvailability.Displayed)
+
+            IWebElement partTimeAvailabilityHolder = driver.FindElement(By.XPath("//div[@class='extra content']/div/div[2]/div/span"));
+            string availabilityValue = partTimeAvailabilityHolder.GetAttribute("innerText");
+
+            if (availabilityValue.Contains("Part Time"))
             {
-                Console.WriteLine("Availability updated to PartTime successfully");
+                Console.WriteLine("Availability updated to PartTime successfully -Verified");
             }
-            IWebElement asNeededHours = driver.FindElement(By.XPath("//span[text()='As needed']"));
-            if (asNeededHours.Displayed)
+
+            IWebElement hoursHolder = driver.FindElement(By.XPath("//div[@class='extra content']/div/div[3]/div/span"));
+            string hoursValue = hoursHolder.GetAttribute("innerText");
+
+
+            if (hoursValue.Contains("As needed"))
             {
-                Console.WriteLine("Hours updated to As Needed successfully");
+                Console.WriteLine("Availability updated to PartTime successfully -Verified");
             }
-            IWebElement MoreThanET = driver.FindElement(By.XPath("//span[text()='More than $1000 per month']"));
-            if (MoreThanET.Displayed)
+
+            IWebElement eTHolder = driver.FindElement(By.XPath("//div[@class='extra content']/div/div[4]/div/span"));
+            string eTValue = eTHolder.GetAttribute("innerText");
+
+            if (eTValue.Contains("More than $1000 per month"))
             {
-                Console.WriteLine("Earn Target updated to More than $1000 per month successfully");
+                Console.WriteLine("Earn Target updated to More than $1000 per month successfully -Verified");
+            }
+        }
+        public void AddDescription(IWebDriver driver)
+        {
+            waitObj.WaitForAjax(driver);
+            //Add description
+            IWebElement descriptionIcon = driver.FindElement(By.XPath("//i[@class='outline write icon']"));
+            descriptionIcon.Click();
+            IWebElement descriptionBox = driver.FindElement(By.XPath("//div[@class='field  ']//textarea"));
+            descriptionBox.Clear();
+            descriptionBox.SendKeys("test description");
+            driver.FindElement(By.XPath("//div[@class='ui twelve wide column']//button")).Click();
+            Console.WriteLine("Added profile description");
+        }
+        public void ProfileDiscriptionSuccessfullyUpdated(IWebDriver driver)
+        {
+            waitObj.WaitForAjax(driver);
+            IWebElement descValue = driver.FindElement(By.XPath("//span[text()='test description']"));
+            if (descValue.Displayed)
+            {
+                Console.WriteLine("Description successfully updated -Verified");
+            }
+        }
+
+        public void AddLanguage(IWebDriver driver)
+        {
+            waitObj.WaitForAjax(driver);
+            IWebElement languagesTab = driver.FindElement(By.XPath("//a[text()='Languages']"));
+            languagesTab.Click();
+            IWebElement landAddNew = driver.FindElement(By.XPath("(//div[text()='Add New'])[1]"));
+            landAddNew.Click();
+            IWebElement langHolder = driver.FindElement(By.XPath("//input[@placeholder='Add Language']"));
+            langHolder.Clear();
+            langHolder.SendKeys("English");
+            //IWebElement ChooseLevelDropDown = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[2]/select"));
+            //ChooseLevelDropDown.Click();
+            IWebElement FluentLevelOption = driver.FindElement(By.XPath("//select//option[@value='Fluent']"));
+            FluentLevelOption.Click();
+            IWebElement AddButton = driver.FindElement(By.XPath("//input[@type='button' and @value='Add']"));
+            AddButton.Click();
+            Console.WriteLine("Language added");
+        }
+
+        public void ProfileLanguageSuccessfullyUpdated(IWebDriver driver)
+        {
+            waitObj.WaitForAjax(driver);
+            IWebElement langInfo = driver.FindElement(By.XPath("//td[text()='English']//following-sibling::td[text()='Fluent']"));
+            if (langInfo.Displayed)
+            {
+                Console.WriteLine("Language successfully updated -Verified");
             }
         }
     }
-
-    
 }
 
